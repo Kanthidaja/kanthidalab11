da0ed7e0fef3e449c617da06b9513f5300bfa32b
kanthidaja <kanthida_khun@cmu..ac.th>
2024-12-20 17:32:14 +0700

    completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -14,15 +14,15 @@ int main(){
     srand(time(0));
     int x = rand()%9;
     string A ;
-    if(x==0) A = 'A' ;
-    else if(x==1) A = 'B+';
-    else if(x==2) A = 'B' ;
-    else if(x==3) A = 'C+' ;
-    else if(x==4) A = 'C' ;
-    else if(x==5) A = 'D+' ;
-    else if(x==6) A = 'D' ;
-    else if(x==7) A = 'F' ;
-    else  A = 'W';
+    if(x==0) A = "A" ;
+    else if(x==1) A = "B+";
+    else if(x==2) A = "B" ;
+    else if(x==3) A = "C+" ;
+    else if(x==4) A = "C" ;
+    else if(x==5) A = "D+" ;
+    else if(x==6) A = "D" ;
+    else if(x==7) A = "F" ;
+    else  A = "W";
     cout << "You will get " << A << " in this 261102.";
 return 0;    
 }
